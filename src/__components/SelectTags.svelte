9381f97fdf1b3b1a68a537135bd985ec0de2c235
Jithu S Nair <work.jithus@gmail.com>
2021-08-20 18:13:42 +0530

    adds more selection options

--- src/__components/SelectTags.svelte
+++ src/__components/SelectTags.svelte
@@ -1,7 +1,9 @@
 <script>
 	import Select from 'svelte-select';
 
-	function loadOptions(filterText) {
+	let loadOptions = loadOptions2;
+
+	function loadOptions1(filterText) {
 		filterText = filterText ? filterText.replace(' ', '_') : '';
 
 		return fetch('https://danbooru.donmai.us/tags.json?search[name_matches]=' + filterText + '*', {
@@ -17,16 +19,63 @@
 			.catch((error) => console.error(error));
 	}
 
+	function loadOptions2(filterText) {
+		filterText = filterText ? filterText.replace(' ', '_') : '';
+
+		return fetch('https://danbooru.donmai.us/tags.json?search[name_matches]=' + filterText, {
+			method: 'GET' // *GET, POST, PUT, DELETE, etc.
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				return data.sort((a, b) => {
+					if (a.name > b.name) return 1;
+					if (a.name < b.name) return -1;
+				});
+			})
+			.catch((error) => console.error(error));
+	}
+
 	const optionIdentifier = 'id';
 	const getOptionLabel = (option) => option.name;
 	const getSelectionLabel = (option) => option.name;
+
+	let selectedItem = 'exact';
+	const items = ['contains', 'exact'];
+
+	function onSelect(event) {
+		if (event.detail === 'contains') {
+			loadOptions = loadOptions1;
+		} else {
+			loadOptions = loadOptions2;
+		}
+	}
 </script>
 
-<Select
-	{loadOptions}
-	{optionIdentifier}
-	{getSelectionLabel}
-	{getOptionLabel}
-	placeholder="Search for tags"
-	on:select
-/>
+<div class="container">
+	<Select
+		{loadOptions}
+		{optionIdentifier}
+		{getSelectionLabel}
+		{getOptionLabel}
+		placeholder="Search for tags"
+		on:select
+	/>
+
+	<Select
+		{items}
+		on:select={onSelect}
+		bind:value={selectedItem}
+		placeholder="Change this first if needed before searching"
+	/>
+
+	<button on:click>Hot</button>
+</div>
+
+<style>
+	.container {
+		display: grid;
+		column-gap: 10px;
+		align-items: center;
+		grid-template-columns: 300px 300px 100px;
+	}
+</style>
